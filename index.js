04770707c3681f49b2179c47b8c8cce95e6a74fc
Brandon Lau <brandonlau@Brandons-MacBook-Pro.local>
2021-02-16 09:05:44 -0500

    player1 validations done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class BattleshipApp {
   turn = "Player1-Turn1";
   player1StartingPoint = [];
   player1EndingPoint = [];
+  gameOver = false;
   virtualBoard = [
     [".", ".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", "."],
@@ -36,17 +37,21 @@ class BattleshipApp {
   addClickEventsToBoard = () => {
     for (let square of ALL_SQUARES) {
       square.addEventListener("click", () => {
-        let clickedRow = square.classList[1][1];
-        let clickedCol = square.classList[1][3];
+        if (!this.gameOver) {
+          let clickedRow = square.classList[1][1];
+          let clickedCol = square.classList[1][3];
 
-        switch (this.turn) {
-          case "Player1-Turn1":
-            this.player1StartingPoint = [clickedRow, clickedCol];
-            this.startPlayer1Turn2();
-            break;
-          case "Player1-Turn2":
-            this.player1EndingPoint = [clickedRow, clickedCol];
-            validatePlayer1Choices();
+          switch (this.turn) {
+            case "Player1-Turn1":
+              this.player1StartingPoint = [clickedRow, clickedCol];
+              square.classList.add("clicked");
+              this.startPlayer1Turn2();
+              break;
+            case "Player1-Turn2":
+              this.player1EndingPoint = [clickedRow, clickedCol];
+              square.classList.add("clicked");
+              this.validatePlayer1Choices();
+          }
         }
       });
     }
@@ -57,8 +62,49 @@ class BattleshipApp {
     GAME_MESSAGE.innerText = "Player 1, now please choose your Starting square";
   };
   startPlayer1Turn2 = () => {
-    this.turn = "Player1-TUrn2";
+    this.turn = "Player1-Turn2";
     GAME_MESSAGE.innerText = "Player 1, please choose your Ending square";
+  };
+  validatePlayer1Choices = () => {
+    let startRow = this.player1StartingPoint[0];
+    let startCol = this.player1StartingPoint[1];
+    let endRow = this.player1EndingPoint[0];
+    let endCol = this.player1EndingPoint[1];
+
+    //Check if Player 1 chose diagonally placed pieces
+    if (
+      Math.abs(endRow - startRow) !== 0 &&
+      Math.abs(endCol - startCol) !== 0
+    ) {
+      this.gameOver = true;
+      alert(
+        "Invalid, please do not choose diagonally. Please refresh and try again"
+      );
+    }
+
+    //Check if Player 1 chose a battleship less than 3 squares long
+    if (Math.max(endRow - startRow, endCol - startCol) < 2) {
+      this.gameOver = true;
+      alert(
+        "Please choose a battleship that is 3 squares long. Please refresh and try again"
+      );
+    }
+
+    //Check if Player 1 chose a battleship more than 3 squares long
+    if (Math.abs(endRow - startRow) > 2 || Math.abs(endCol - startCol) > 2) {
+      this.gameOver = true;
+      alert(
+        "Please do not choose a battleship longer than 3 squares long. Please refresh and try again"
+      );
+      this.updateVirtualBoardWithPlayer1Choices();
+    }
+
+    updateVirtualBoardWithPlayer1Choices = () => {
+      let startRow = this.player1StartingPoint[0];
+      let startCol = this.player1StartingPoint[1];
+      let endRow = this.player1EndingPoint[0];
+      let endCol = this.player1EndingPoint[1];
+    };
   };
 }
 
